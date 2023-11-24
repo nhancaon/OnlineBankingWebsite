d716a68639f7b9cdc55f6eef9474c68ea6a1b549
iamt4jdd <chgdat10a3@gmail.com>
2023-11-24 10:39:22 +0700

    Create Hash Password

--- src/java/business/Customer.java
+++ src/java/business/Customer.java
@@ -14,7 +14,7 @@ import javax.persistence.Table;
 @Table(name = "customer")
 @AttributeOverride(name = "name", column = @Column(columnDefinition = "varchar(100)", nullable = false))
 @AttributeOverride(name = "dateofBirth", column = @Column(columnDefinition = "varchar(20)", nullable = false))
-@AttributeOverride(name = "password", column = @Column(columnDefinition = "varchar(20)", nullable = false))
+@AttributeOverride(name = "password", column = @Column(columnDefinition = "varchar(100)", nullable = false))
 @AttributeOverride(name = "email", column = @Column(columnDefinition = "varchar(100)", nullable = false))
 @AttributeOverride(name = "phoneNumber", column = @Column(columnDefinition = "varchar(20)", nullable = false))
 @AttributeOverride(name = "address", column = @Column(columnDefinition = "varchar(100)", nullable = false))
